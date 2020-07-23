d832b44245f6132accffd289811262b2d80a9938
Megan Yu <megan2521@gmail.com>
2020-07-22 21:04:07 -0700

    Added useful functions to Round model to be able to mark a round as complete or extend the end time for a round

--- Prolific/Prolific/Models/Round/Round.h
+++ Prolific/Prolific/Models/Round/Round.h
@@ -37,6 +37,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (instancetype)new NS_UNAVAILABLE;
 
+/** Only returns true if round contains at least one submission, hasn't already been marked as complete, and current time is past specified round end time. */
+- (BOOL)needToMarkAsComplete;
+
+/** Only returns true if round contains no submissions, hasn't been marked as complete, and current time is past specified round end time. */
+- (BOOL)needToExtendTime;
+
+/** Only extends the end time by 1 day if there are no submissions in the round. */
+- (BOOL)extendEndTime;
+
+/** Only marks a round as complete and sets a winning snippet Id if the round is ready and hasn't already been marked as complete. */
+- (BOOL)markCompleteAndSetWinningSnippet;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Prolific/Prolific/Models/Round/Round.m
+++ Prolific/Prolific/Models/Round/Round.m
@@ -8,6 +8,8 @@
 
 #import "Round.h"
 
+@import Firebase;
+
 @implementation Round
 
 #pragma mark - Initializer
@@ -23,6 +25,62 @@
         _winningSnippetId = builder.winningSnippetId;
     }
     return self;
+}
+
+/** Only returns true if round contains at least one submission, hasn't already been marked as complete, and current time is past specified round end time. */
+- (BOOL)needToMarkAsComplete {
+    NSDate *const currTime = [FIRTimestamp timestamp].dateValue;
+    if (!_isComplete &&
+        _submissions.count > 0 &&
+        [currTime timeIntervalSinceDate:_endTime] > 0) {
+        return YES;
+    }
+    return NO;
+}
+
+/** Only returns true if round contains no submissions, hasn't been marked as complete, and current time is past specified round end time. */
+- (BOOL)needToExtendTime {
+    NSDate *const currTime = [FIRTimestamp timestamp].dateValue;
+    if (!_isComplete &&
+        _submissions.count == 0 &&
+        [currTime timeIntervalSinceDate:_endTime] > 0) {
+        return YES;
+    }
+    return NO;
+}
+
+/** Only extends the end time by 1 day if there are no submissions in the round. */
+- (BOOL)extendEndTime {
+    if (_submissions.count == 0) {
+        NSDateComponents *const dayComponent = [[NSDateComponents alloc] init];
+        dayComponent.day = 1;
+        NSLog(@"Extending deadline! End time is originally: %@", _endTime);
+        NSCalendar *const currCalendar = [NSCalendar currentCalendar];
+        NSDate *const extendedEndTime = [currCalendar dateByAddingComponents:dayComponent toDate:_endTime options:0];
+        
+        _endTime = extendedEndTime;
+        NSLog(@"New end time is: %@", _endTime);
+        return YES;
+    }
+    return NO;
+}
+
+/** Only marks a round as complete and sets a winning snippet Id if the round is ready and hasn't already been marked as complete. */
+- (BOOL)markCompleteAndSetWinningSnippet {
+    if ([self needToMarkAsComplete]) {
+        _isComplete = YES;
+        
+        Snippet *winningSnippetSoFar = _submissions[0];
+        
+        for (Snippet *const snippet in _submissions) {
+            if (snippet.voteCount > winningSnippetSoFar.voteCount) {
+                winningSnippetSoFar = snippet;
+            }
+        }
+        _winningSnippetId = winningSnippetSoFar.snippetId;
+        return YES;
+    }
+    return NO;
 }
 
 @end
