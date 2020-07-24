16cd852cfb65e211e9e1ad5c1732f2da023d5b55
Megan Yu <megan2521@gmail.com>
2020-07-23 17:33:53 -0700

    Modified RoundCell to load snippet information into a RoundCellView

--- Prolific/Prolific/Views/Cells/Round Cell/RoundCell.m
+++ Prolific/Prolific/Views/Cells/Round Cell/RoundCell.m
@@ -10,10 +10,42 @@
 
 @implementation RoundCell
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _cellView = [[RoundCellView alloc] initWithFrame:frame];
+        [self.contentView addSubview:_cellView];
+    }
+    return self;
+}
+
+#pragma mark - Setup
+
 - (void)layoutSubviews {
     [super layoutSubviews];
     
-    // TODO: add other labels to Round cell
+    _cellView.frame = self.contentView.bounds;
+    
+    if (_snippet) {
+        _cellView.usernameLabel.hidden = NO;
+        _cellView.voteCountLabel.hidden = NO;
+        _cellView.voteButton.hidden = NO;
+        
+        _cellView.usernameLabel.text = _snippet.authorId;
+        _cellView.seedContentLabel.text = _snippet.text;
+        _cellView.voteCountLabel.text = [_snippet.voteCount stringValue];
+    } else {
+        _cellView.usernameLabel.hidden = YES;
+        _cellView.voteCountLabel.hidden = YES;
+        _cellView.voteButton.hidden = YES;
+        
+        _cellView.seedContentLabel.text = @"Voting in progress!";
+    }
+}
+
+- (void)setSnippet:(Snippet *)snippet {
+    _snippet = snippet;
 }
 
 @end
