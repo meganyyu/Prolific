71e2b3d746e1b8f7415454f6f3c4b46f87c22047
Megan Yu <megan2521@gmail.com>
2020-07-23 17:42:29 -0700

    Changed ProjectDetailsVC to use a collection view that loads with ProjectCells and RoundCells. Modified refreshData method to reload collection view at appropriate times.

--- Prolific/Prolific/View Controllers/Project Details View Controller/ProjectDetailsViewController.m
+++ Prolific/Prolific/View Controllers/Project Details View Controller/ProjectDetailsViewController.m
@@ -24,10 +24,7 @@ static NSString *const kRoundComposeIconId = @"round-compose-icon";
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) DAO *dao;
 @property (nonatomic, strong) UIView *projectView;
-@property (nonatomic, strong) UILabel *nameLabel;
-@property (nonatomic, strong) UILabel *seedContentLabel;
 @property (nonatomic, strong) UIButton *composeButton;
-@property (nonatomic, strong) UIButton *previewButton;
 
 @end
 
@@ -45,49 +42,40 @@ static NSString *const kRoundComposeIconId = @"round-compose-icon";
     self.navigationItem.title = @"Project Details";
     [super setupBackButton];
     
-    _collectionView.dataSource = self;
-    _collectionView.delegate = self;
-    [_collectionView registerClass:[RoundCell class]
-        forCellWithReuseIdentifier:@"roundCell"];
-    [_collectionView setBackgroundColor:[UIColor ProlificBackgroundGrayColor]];
-    
-    [self.view addSubview:_collectionView];
-    
+    [self setupCollectionView];
     // TODO: turn into collection view
     
     _projectView = [[UIView alloc] init];
     [self.view addSubview:_projectView];
 
-    _nameLabel = [[UILabel alloc] init];
-    _nameLabel.textColor = [UIColor blackColor];
-    _nameLabel.numberOfLines = 0;
-    [_projectView addSubview:_nameLabel];
-
-    _seedContentLabel = [[UILabel alloc] init];
-    _seedContentLabel.textColor = [UIColor blackColor];
-    _seedContentLabel.numberOfLines = 0;
-    [_projectView addSubview:_seedContentLabel];
-    
     _composeButton = [[UIButton alloc] init];
-    _composeButton.backgroundColor = [UIColor ProlificPrimaryBlueColor];
+    [_composeButton setImage:[UIImage imageNamed:kRoundComposeIconId] forState:normal];
     _composeButton.tintColor = [UIColor whiteColor];
-    [_composeButton setTitle:@"Submit a snippet!" forState:normal];
     [_composeButton addTarget:self
                        action:@selector(onTapCompose:)
              forControlEvents:UIControlEventTouchUpInside];
     [_projectView addSubview:_composeButton];
     
-    _previewButton = [[UIButton alloc] init];
-    _previewButton.backgroundColor = [UIColor ProlificGray2Color];
-    _previewButton.tintColor = [UIColor whiteColor];
-    [_previewButton setTitle:@"See submitted snippets for latest round"
-                    forState:normal];
-    [_previewButton addTarget:self
-                       action:@selector(onTapPreview:)
-             forControlEvents:UIControlEventTouchUpInside];
-    [_projectView addSubview:_previewButton];
-    
     [self refreshData];
+}
+
+- (void)setupCollectionView {
+    UICollectionViewFlowLayout *const layout = [[UICollectionViewFlowLayout alloc] init];
+    _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds
+                                         collectionViewLayout:layout];
+    
+    _collectionView.dataSource = self;
+    _collectionView.delegate = self;
+    
+    [_collectionView registerClass:[RoundCell class]
+        forCellWithReuseIdentifier:@"roundCell"];
+    [_collectionView registerClass:[ProjectCell class]
+    forCellWithReuseIdentifier:@"projectCell"];
+    
+    [_collectionView setBackgroundColor:[UIColor ProlificBackgroundGrayColor]];
+    
+    [self.view addSubview:_collectionView];
+
 }
 
 - (void)viewDidLayoutSubviews {
@@ -100,33 +88,15 @@ static NSString *const kRoundComposeIconId = @"round-compose-icon";
     // project view
     _projectView.frame = CGRectMake(0, 0, boundsWidth, boundsHeight);
     
-    // project name label
-    CGFloat const labelWidth = 0.9 * boundsWidth;
-    CGFloat const labelX = 0.05 * boundsWidth;
-    CGFloat const nameLabelHeight = 0.2 * boundsHeight;
-    CGFloat const nameLabelY = 0.05 * boundsHeight;
-    _nameLabel.frame = CGRectMake(labelX, nameLabelY, labelWidth, nameLabelHeight);
-    
-    // seed content label
-    CGFloat const seedContentLabelHeight = 0.6 * boundsHeight;
-    CGFloat const seedContentLabelY = nameLabelHeight + 0.05 * boundsHeight;
-    _seedContentLabel.frame = CGRectMake(labelX, seedContentLabelY, labelWidth, seedContentLabelHeight);
-    
     // compose button
-    CGFloat const composeButtonX = _projectView.center.x - 150;
-    CGFloat const composeButtonY = boundsHeight - 300;
-    _composeButton.frame = CGRectMake(composeButtonX, composeButtonY, 300, 30);
-    
-    // preview button
-    CGFloat const previewButtonX = _projectView.center.x - 200;
-    CGFloat const previewButtonY = boundsHeight - 200;
-    _previewButton.frame = CGRectMake(previewButtonX, previewButtonY, 400, 30);
+    CGFloat const composeButtonWidth = 100;
+    CGFloat const composeButtonHeight = 100;
+    CGFloat const composeButtonX = _projectView.center.x - composeButtonWidth / 2.0;
+    CGFloat const composeButtonY = boundsHeight - composeButtonHeight * 2.0;
+    _composeButton.frame = CGRectMake(composeButtonX, composeButtonY, composeButtonWidth, composeButtonHeight);
 }
 
 - (void)refreshData {
-    _nameLabel.text = _project.name;
-    _seedContentLabel.text = _project.seed;
-    
     __weak typeof (self) weakSelf = self;
     [_dao getAllRoundsForProjectId:_project.projectId
                         completion:^(NSMutableArray * _Nonnull rounds, NSError * _Nonnull error) {
@@ -175,14 +145,13 @@ static NSString *const kRoundComposeIconId = @"round-compose-icon";
                             } else {
                                 NSLog(@"Failed to start a new round, try again.");
                             }
-                        }];
-                        
-                        [strongSelf.dao updateExistingRound:updatedLatestRound
-                                         forProjectId:strongSelf.project.projectId
-                                           completion:^(NSError * _Nonnull error) {
-                            if (error) {
-                                NSLog(@"Error marking round as complete.");
-                            }
+                            
+                            dispatch_async(dispatch_get_main_queue(), ^{
+                                typeof(self) strongSelf = weakSelf;
+                                if (strongSelf) {
+                                    [strongSelf.collectionView reloadData];
+                                }
+                            });
                         }];
                     } else if (roundBuilderExtendedTime) {
                         Round *const extendedLatestRound = [roundBuilder build];
@@ -201,6 +170,13 @@ static NSString *const kRoundComposeIconId = @"round-compose-icon";
                     NSLog(@"Error retrieving submissions for latestRound: %@", error.localizedDescription);
                 }
             }];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                typeof(self) strongSelf = weakSelf;
+                if (strongSelf) {
+                    [strongSelf.collectionView reloadData];
+                }
+            });
         } else {
             NSLog(@"Failed to load rounds for project");
         }
@@ -221,7 +197,7 @@ static NSString *const kRoundComposeIconId = @"round-compose-icon";
     }
 }
 
-- (void)onTapPreview:(id)sender {
+- (void)didTapPreview {
     int latestRoundNumber = (int) _project.rounds.count - 1;
     if (latestRoundNumber >= 0) {
         Round *const currentRound = _project.rounds[latestRoundNumber];
@@ -243,14 +219,64 @@ static NSString *const kRoundComposeIconId = @"round-compose-icon";
 
 #pragma mark - UICollectionViewDataSource Protocol
 
-- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView
-                                   cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    return nil;
-}
-
 - (NSInteger)collectionView:(nonnull UICollectionView *)collectionView
      numberOfItemsInSection:(NSInteger)section {
-    return 0;
+    return _project.rounds.count;
+}
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView
+                                   cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    if (indexPath.item == 0) {
+        ProjectCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"projectCell"
+                                                                      forIndexPath:indexPath];
+        cell.project = _project;
+        
+        return cell;
+    } else {
+        RoundCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"roundCell"
+                                                                    forIndexPath:indexPath];
+        
+        Round *const round = _project.rounds[indexPath.item];
+        if (round.winningSnippetId) {
+            [_dao getSubmissionWithId:round.winningSnippetId
+                           forRoundId:round.roundId
+                            projectId:_project.projectId
+                           completion:^(Snippet *snippet, NSError *error) {
+                snippet? [cell setSnippet:snippet] : [cell setSnippet:nil];
+                
+                __weak typeof(self) weakSelf = self;
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    typeof(self) strongSelf = weakSelf;
+                    if (strongSelf) {
+                        [cell setNeedsLayout];
+                    }
+                });
+            }];
+        } else {
+            [cell setSnippet:nil];
+        }
+        
+        return cell;
+    }
+}
+
+#pragma mark - UICollectionViewDelegate Protocol
+
+- (void)collectionView:(UICollectionView *)collectionView
+didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    [collectionView deselectItemAtIndexPath:indexPath animated:YES];
+    
+    if (indexPath.item == _project.rounds.count) {
+        [self didTapPreview];
+    }
+}
+
+#pragma mark - UICollectionViewDelegateFlowLayout Protocol
+
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(collectionView.frame.size.width - 50, collectionView.frame.size.height / 7.0);
 }
 
 @end
