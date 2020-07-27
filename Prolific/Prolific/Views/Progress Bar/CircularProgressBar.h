97f2c121118abc4c2efbe852cba212fe7e68f8e3
Megan Yu <megan2521@gmail.com>
2020-07-27 14:22:34 -0700

    Added a reusable Circular Progress Bar

---
+++ Prolific/Prolific/Views/Progress Bar/CircularProgressBar.h
@@ -0,0 +1,19 @@
+//
+//  CircularProgressBar.h
+//  Prolific
+//
+//  Created by meganyu on 7/27/20.
+//  Copyright © 2020 meganyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CircularProgressBar : UIView
+
+@property (nonatomic) NSInteger progress;
+
+@end
+
+NS_ASSUME_NONNULL_END
