ec5a3b38d4f3cb4ed7853fec9007d668898163b0
Megan Yu <megan2521@gmail.com>
2020-08-01 00:37:37 -0700

    Modified profile image to be rounded into circle with white border. Added backdrop and realigned/reformatted labels.

--- Prolific/Prolific/View Controllers/Profile View Controller/ProfileViewController.m
+++ Prolific/Prolific/View Controllers/Profile View Controller/ProfileViewController.m
@@ -19,6 +19,7 @@ static NSString *const kLoadingAnimationId = @"6541-loading";
 
 @interface ProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
+@property (nonatomic, strong) UIView *backdropView;
 @property (nonatomic, strong) DAO *dao;
 @property UIImagePickerController *imagePickerVC;
 @property (nonatomic, strong) UIView *profileView;
@@ -37,6 +38,8 @@ static NSString *const kLoadingAnimationId = @"6541-loading";
     
     _dao = [[DAO alloc] init];
     
+    self.view.backgroundColor = [UIColor whiteColor];
+    
     self.navigationItem.title = @"Profile";
     [super setupBackButton];
     
@@ -44,6 +47,10 @@ static NSString *const kLoadingAnimationId = @"6541-loading";
     
     _profileView = [[UIView alloc] init];
     [self.view addSubview:_profileView];
+    
+    _backdropView = [[UIView alloc] init];
+    _backdropView.backgroundColor = [UIColor ProlificPrimaryBlueColor];
+    [_profileView addSubview:_backdropView];
     
     _profileImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:kProfileIconId]];
     UITapGestureRecognizer *const profileImageTapGestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self
@@ -67,15 +74,12 @@ static NSString *const kLoadingAnimationId = @"6541-loading";
     self.loadingView.hidden = YES;
     
     _usernameLabel = [[UILabel alloc] init];
-    _usernameLabel.text = _user.username;
     [_profileView addSubview:_usernameLabel];
     
     _displayNameLabel = [[UILabel alloc] init];
-    _displayNameLabel.text = _user.displayName;
     [_profileView addSubview:_displayNameLabel];
     
     _karmaLabel = [[UILabel alloc] init];
-    _karmaLabel.text = [_user.karma stringValue];
     [_profileView addSubview:_karmaLabel];
 }
 
@@ -89,32 +93,44 @@ static NSString *const kLoadingAnimationId = @"6541-loading";
     // profile view
     _profileView.frame = CGRectMake(0, 0, boundsWidth, boundsHeight);
     
+    // background view
+    _backdropView.frame = CGRectMake(0, 0, boundsWidth, 0.3 * boundsHeight);
+    
     // profile picture
-    CGFloat const imageViewWidth = 0.4 * boundsWidth;
+    CGFloat const imageViewWidth = 100;
     CGFloat const imageViewHeight = imageViewWidth;
-    CGFloat const imageViewX = 0.1 * boundsWidth;
-    CGFloat const imageViewY = 0.2 * boundsHeight;
+    CGFloat const imageViewX = _profileView.center.x - imageViewWidth / 2.0;
+    CGFloat const imageViewY = _backdropView.bounds.size.height - imageViewHeight / 2.0;
     _profileImageView.frame = CGRectMake(imageViewX, imageViewY, imageViewWidth, imageViewHeight);
+    _profileImageView.layer.borderWidth = 3.0f;
+    _profileImageView.layer.borderColor = [UIColor whiteColor].CGColor;
+    _profileImageView.layer.cornerRadius = _profileImageView.frame.size.width / 2.0;
+    _profileImageView.clipsToBounds = YES;
     
     // loading view
-    CGFloat const loadingViewWidth = imageViewWidth;
-    CGFloat const loadingViewHeight = imageViewHeight;
-    CGFloat const loadingViewX = imageViewX;
-    CGFloat const loadingViewY = imageViewY;
-    _loadingView.frame = CGRectMake(loadingViewX, loadingViewY, loadingViewWidth, loadingViewHeight);
+    _loadingView.frame = CGRectMake(imageViewX, imageViewY, imageViewWidth, imageViewHeight);
     
     // user info labels
-    CGFloat const labelX = imageViewX;
-    CGFloat const labelWidth = 80;
-    CGFloat const labelHeight = 20;
+    CGFloat const labelWidth = 200;
+    CGFloat const labelHeight = 25;
+    CGFloat const labelX = _profileImageView.center.x - labelWidth / 2.0;
     CGFloat const displayNameLabelY = imageViewY + imageViewHeight + 20;
     _displayNameLabel.frame = CGRectMake(labelX, displayNameLabelY, labelWidth, labelHeight);
+    _displayNameLabel.text = _user.displayName;
+    _displayNameLabel.textAlignment = NSTextAlignmentCenter;
+    [_displayNameLabel setFont:[UIFont boldSystemFontOfSize:22]];
     
     CGFloat const usernameLabelY = displayNameLabelY + labelHeight + 8;
     _usernameLabel.frame = CGRectMake(labelX, usernameLabelY, labelWidth, labelHeight);
+    _usernameLabel.text = [NSString stringWithFormat:@"@%@", _user.username];
+    _usernameLabel.textAlignment = NSTextAlignmentCenter;
+    [_usernameLabel setFont:[UIFont systemFontOfSize:14]];
     
-    CGFloat const karmaLabelY = usernameLabelY + labelHeight + 8;
+    CGFloat const karmaLabelY = usernameLabelY + labelHeight + 20;
     _karmaLabel.frame = CGRectMake(labelX, karmaLabelY, labelWidth, labelHeight);
+    _karmaLabel.text = [NSString stringWithFormat:@"Karma: %@", [_user.karma stringValue]];
+    _karmaLabel.textAlignment = NSTextAlignmentCenter;
+    [_karmaLabel setFont:[UIFont systemFontOfSize:18]];
 }
 
 - (void)setupImagePicker {
