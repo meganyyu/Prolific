8d457c5972aec5507f62d9d1223eb21cbc4eb63f
Megan Yu <megan2521@gmail.com>
2020-08-07 14:24:02 -0700

    Initialized basic UI elements for CreateProjectViewController and set up navbar buttons

--- Prolific/Prolific/View Controllers/Create Project View Controller/CreateProjectViewController.m
+++ Prolific/Prolific/View Controllers/Create Project View Controller/CreateProjectViewController.m
@@ -21,6 +21,9 @@ static NSString *const kSubmitIconId = @"submit-icon";
 
 @interface CreateProjectViewController ()
 
+@property (nonatomic, strong) UIView *composeView;
+@property (nonatomic, strong) UITextView *seedTextView;
+@property (nonatomic, strong) UITextField *titleTextField;
 @property (nonatomic, strong) DAO *dao;
 
 @end
@@ -39,6 +42,53 @@ static NSString *const kSubmitIconId = @"submit-icon";
         _dao = [[DAO alloc] init];
     }
     return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"Create Project";
+    [super setupBackButton];
+    [self setupSubmitButton];
+    
+    _composeView = [[UIView alloc] init];
+    [self.view addSubview:_composeView];
+    
+    _titleTextField = [[UITextField alloc] init];
+    [_composeView addSubview:_titleTextField];
+    [_titleTextField becomeFirstResponder];
+    
+    _seedTextView = [[UITextView alloc] init];
+    _seedTextView.backgroundColor = [UIColor ProlificBackgroundGrayColor];
+    _seedTextView.textColor = [UIColor blackColor];
+    [_composeView addSubview:_seedTextView];
+}
+
+- (void)setupSubmitButton {
+    UIButton *const submitButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    submitButton.frame = CGRectMake(0, 0, 40, 40);
+    [submitButton setImage:[[UIImage imageNamed:kSubmitIconId] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]
+                forState:UIControlStateNormal];
+    [submitButton addTarget:self
+                     action:@selector(onTapCreate:)
+           forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:submitButton];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [_titleTextField endEditing:YES];
+    [_seedTextView endEditing:YES];
+}
+
+#pragma mark - User Actions
+
+- (void)onTapBack:(id)sender{
+    [NavigationManager exitViewController:self.navigationController];
+}
+
+- (void)onTapCreate:(id)sender{
+    
 }
 
 @end
