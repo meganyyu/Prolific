ee4300807c43ffa0525c925b8d606db9165d36a1
Megan Yu <megan2521@gmail.com>
2020-08-07 14:53:06 -0700

    Added layout code and placeholder text for title text field and seed text view, added bottom border to title text field.

--- Prolific/Prolific/View Controllers/Create Project View Controller/CreateProjectViewController.m
+++ Prolific/Prolific/View Controllers/Create Project View Controller/CreateProjectViewController.m
@@ -55,13 +55,18 @@ static NSString *const kSubmitIconId = @"submit-icon";
     [self.view addSubview:_composeView];
     
     _titleTextField = [[UITextField alloc] init];
+    _titleTextField.textAlignment = NSTextAlignmentCenter;
+    _titleTextField.placeholder = @"Untitled";
+    _titleTextField.font = [UIFont systemFontOfSize:24];
     [_composeView addSubview:_titleTextField];
-    [_titleTextField becomeFirstResponder];
     
     _seedTextView = [[UITextView alloc] init];
     _seedTextView.backgroundColor = [UIColor ProlificBackgroundGrayColor];
     _seedTextView.textColor = [UIColor blackColor];
+    _seedTextView.font = [UIFont systemFontOfSize:18];
+    _seedTextView.text = @"What happens first?";
     [_composeView addSubview:_seedTextView];
+    [_seedTextView becomeFirstResponder];
 }
 
 - (void)setupSubmitButton {
@@ -73,6 +78,40 @@ static NSString *const kSubmitIconId = @"submit-icon";
                      action:@selector(onTapCreate:)
            forControlEvents:UIControlEventTouchUpInside];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:submitButton];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    CGRect const bounds = self.view.bounds;
+    CGFloat const boundsWidth = CGRectGetWidth(bounds);
+    CGFloat const boundsHeight = CGRectGetHeight(bounds);
+    
+    // submission view
+    _composeView.frame = CGRectMake(0, 0, boundsWidth, boundsHeight);
+    
+    // title text field
+    CGFloat const titleFieldX = 0.05 * boundsWidth;
+    CGFloat const titleFieldY = self.navigationController.navigationBar.frame.size.height + 0.1 * boundsHeight;
+    CGFloat const titleFieldWidth = 0.9 * boundsWidth;
+    CGFloat const titleFieldHeight = 0.05 * boundsHeight;
+    _titleTextField.frame = CGRectMake(titleFieldX, titleFieldY, titleFieldWidth, titleFieldHeight);
+    
+    // title text field's bottom border
+    CALayer *const border = [CALayer layer];
+    CGFloat borderWidth = 1;
+    border.borderColor = [UIColor ProlificGray2Color].CGColor;
+    border.frame = CGRectMake(0, _titleTextField.frame.size.height - borderWidth, _titleTextField.frame.size.width, _titleTextField.frame.size.height);
+    border.borderWidth = borderWidth;
+    [_titleTextField.layer addSublayer:border];
+    _titleTextField.layer.masksToBounds = YES;
+    
+    // submission text view
+    CGFloat const textViewX = titleFieldX;
+    CGFloat const textViewY = titleFieldY + titleFieldHeight + textViewX;
+    CGFloat const textViewWidth = titleFieldWidth;
+    CGFloat const textViewHeight = boundsHeight - textViewY;
+    _seedTextView.frame = CGRectMake(textViewX, textViewY, textViewWidth, textViewHeight);
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
