033ae9bf53f881d3fee764d249a1f6e547987de8
Megan Yu <megan2521@gmail.com>
2020-08-11 12:45:43 -0700

    Created an empty FeedCell to use in ProfileVC

---
+++ Prolific/Prolific/Views/Cells/Feed Cell/FeedCell.m
@@ -0,0 +1,13 @@
+//
+//  FeedCell.m
+//  Prolific
+//
+//  Created by meganyu on 8/11/20.
+//  Copyright © 2020 meganyu. All rights reserved.
+//
+
+#import "FeedCell.h"
+
+@implementation FeedCell
+
+@end
